1b2b5abeb5e8357db284ecbb6bc9d815f7262ff1
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-12-09 14:01:25 -0300

    i2c test

--- bus_teste.py
+++ bus_teste.py
@@ -3,5 +3,6 @@ from smbus2 import SMBus
 # Replace 0x27 with the actual I2C address of your device
 address = 0x27
 
-bus = SMBus(1)
+with SMBus(1) as bus:
+    bus.write_byte(address, 0x01)
 print(bus)
