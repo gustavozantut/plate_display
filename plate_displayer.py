55c10a9a86112b1db6f8697d1033c8bcf2617bc2
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-12-10 16:40:07 -0300

    logging

--- plate_displayer.py
+++ plate_displayer.py
@@ -2,6 +2,29 @@ from confluent_kafka import Consumer, KafkaError
 import time
 import json
 import serial
+import logging
+
+def configure_logging():
+    # Create a logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Create a file handler
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 arduino_port = '/dev/ttyACM0'
 ser = serial.Serial(arduino_port, 9600, timeout=5)
@@ -44,9 +67,9 @@ try:
         lcd_message = msg.value().decode('utf-8')
         lcd_message = json.loads(lcd_message)
         lcd_message = '     '+lcd_message["results"][0]["plate"]+'\n'
-        print(f"sending plate {lcd_message} to ino")
+        logging.info(f"sending plate {lcd_message} to ino")
         display_on_lcd(message=lcd_message)
-        print(f"plate sent")
+        logging.info(f"plate sent")
 
 except KeyboardInterrupt:
     
